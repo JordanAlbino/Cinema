6e15ea09c0ff46a8d5fc26c5e9bebfbbd6ab802b
Jordan Lima <jordan.albino@aluno.senai.br>
2024-11-13 20:43:46 -0300

    erro corrigido

--- cinema/src/main/java/com/teste/cinema/model/Assento.java
+++ cinema/src/main/java/com/teste/cinema/model/Assento.java
@@ -1,6 +1,5 @@
 package com.teste.cinema.model;
 
-<<<<<<< HEAD
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,8 +10,4 @@ public class Assento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-=======
-public class Assento {
-    
->>>>>>> ba8c57b51855ab2372fcc6e26392ecdf7d16b9f4
 }
